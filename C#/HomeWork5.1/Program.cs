6f4d7b65e9e497bbb1eb9d00694ce9eac8b539f8
Maksym <stefurakmax2005@gmail.com>
2024-10-22 20:26:08 +0300

    homework5.1

---
+++ C#/HomeWork5.1/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        try
+        {
+
+            Console.WriteLine("Enter First Number:");
+            decimal firstNumber = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter Secound Number:");
+            decimal secoundNumber = Convert.ToDecimal(Console.ReadLine());
+
+            
+            decimal result = firstNumber / secoundNumber;
+
+           
+            Console.WriteLine($"Result: {firstNumber} / {secoundNumber} = {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            
+            Console.WriteLine("Error: Division by zero is not possible.");
+        }
+        catch (FormatException)
+        {
+            
+            Console.WriteLine("\r\nError: Incorrect value entered. Enter the numbers.");
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+        finally
+        {
+            
+            Console.WriteLine("\r\nThe operation is complete.");
+        }
+    }
+}
+    
